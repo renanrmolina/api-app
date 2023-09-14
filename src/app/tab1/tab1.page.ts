a5c3d7f80f9cb14ee1078165e76919f8ddd5273f
Renan Molina <renanrobertomolina@hotmail.com>
2023-09-14 20:28:27 -0300

    14-09
    Ligamos o projeto com a nossa api e exibimos os dados na tela.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from '../services/usuario.service';
+import { Usuario } from '../Models/Usuario.model';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  listaUsuarios: Usuario[] = [];
+  usuario?: Usuario;
+  id: number = 0;
 
+  /*
+    *Para buscar dados da API, depois de criar o serviço temos que instanciar em uma variavel o serviço criado
+  */
+  constructor(private userService: UsuarioService) {}
+
+  //Criar os métodos que conversam com a API
+
+  buscarUsuarios(){
+    //Primeiro chamamos o método do serviço
+    //Depois adicionamos o subscribe para receber
+    // a resposta quando ela chegar
+
+    // adicionamos uma variavel que guarda o retorno
+    //e enviamos ela para uma função anônima
+    // dentro da função vamos adicionar o retorno a variavel
+    // local
+    this.userService.getAll().subscribe(retorno =>{
+      // "as Usuario[]" tentar converter o retorno para este tipo
+      this.listaUsuarios = retorno as Usuario[];
+      console.log(this.listaUsuarios);
+      this.usuario = undefined;
+    });
+  }
+
+  buscarPorID(){
+    this.userService.getOne(this.id).subscribe(retorno =>{
+      console.log(retorno);
+      this.usuario = retorno as Usuario;
+      this.listaUsuarios = [];
+    });
+  }
 }
